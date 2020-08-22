a9b57c8231c6f8ead848bf622c066bfacfe6579d
xsummersx <605369747@qq.com>
2020-08-22 10:49:57 +0800

    项目目录结构创建

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
-
+Vue.use(VueAxios, axios)
+Vue.axios.defaults.baseURL = ''
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
