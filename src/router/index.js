e4bc206b9ca62f4a51acc4d716a092f7063cdd22
xsummersx <605369747@qq.com>
2020-08-22 11:39:14 +0800

    调整router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,17 +7,29 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue'),
+    component: () => import('../views/Home.vue'),
+    children: [
+      {
+        path: '/index',
+        name: 'index',
+        component: () => import('../views/index.vue'),
+      },
+      {
+        path: '/product/:id',
+        name: 'product',
+        component: () => import('../views/product.vue'),
+      },
+      {
+        path: '/detail/:id',
+        name: 'detail',
+        component: () => import('../views/detail.vue'),
+      },
+    ],
   },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes,
 })
